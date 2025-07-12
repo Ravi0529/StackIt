49608bd453e6fcde42da94692e59c4676fae7be1
Govindggupta <govind12771277@gmail.com>
2025-07-12 08:42:45 +0000

    backend routes

---
+++ src/app/api/question/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+
+// let user create new question here 
+
+async function POST(req: NextRequest, res: NextResponse) {
+
+    
+}
