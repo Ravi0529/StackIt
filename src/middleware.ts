569086eeeee5cc807ac4fed5e8c1acc24c5b2299
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-07-22 11:07:37 +0530

    middleware-update

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,21 +8,26 @@ export const middleware = async (req: NextRequest) => {
 
   if (
     token &&
-    (url.pathname.startsWith("/signin") ||
-      url.pathname.startsWith("/signup") ||
+    (url.pathname === "/signin" ||
+      url.pathname === "/signup" ||
       url.pathname === "/")
   ) {
     return NextResponse.redirect(new URL("/feed", req.url));
   }
 
-  if (!token && url.pathname.startsWith("/feed")) {
+  if (url.pathname === "/feed") {
+    return NextResponse.next();
+  }
+
+  if (!token && url.pathname.startsWith("/feed/")) {
     return NextResponse.redirect(new URL("/signin", req.url));
   }
 
   return NextResponse.next();
 };
 
+// Apply middleware to these paths
 // the config.matcher field tells Next.js which routes should run this middleware
 export const config = {
-  matcher: ["/signin", "/signup", "/", "/feed/:path*"],
+  matcher: ["/signin", "/signup", "/", "/feed", "/feed/:path*"],
 };
