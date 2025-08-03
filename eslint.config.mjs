08c2f9a5ff061427425d7ef01d9c9ca0f345ac4d
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-08-03 20:23:50 +0530

    deployment-fixes(#6)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,22 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  js.configs.recommended,
+
+  ...compat.extends("next/core-web-vitals"),
+
+  ...compat.extends("plugin:@typescript-eslint/recommended"),
+
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-non-null-assertion": "warn",
+      "no-console": "warn",
+      "react/prop-types": "off",
+      "@typescript-eslint/no-implicit-any": "error"
+    }
+  }
 ];
 
 export default eslintConfig;
